cc53ad428ae656835a93014144cb8fc7aa0e9242
Amaithi <amaithichirasan.s@gmail.com>
2023-05-04 16:22:28 +0530

    setup.py done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="it's a MLOPs Package of wineq",
+    Author="Amaithi Chirasan S",
+    packages=find_packages(),
+    license="MIT"
+)
